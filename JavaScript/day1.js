6da4d74c9207dcc49490e7fadc323790eba209ab
Suhas-MSRUAS <158500939+Suhas-MSRUAS@users.noreply.github.com>
2025-03-26 01:50:02 +0530

    Create day1.js

---
+++ JavaScript/day1.js
@@ -0,0 +1,21 @@
+/*console.log(typeof '1');
+console.log(typeof console)
+console.log(typeof log)
+console.log(5+5)
+console.log(5+'5')
+console.log(5-1)
+console.log(5-'5')
+
+
+var b=null
+console.log(null)
+
+
+var a=10;
+var b=20;
+var c=a+b;
+console.log(`the sum of ${a} and ${b} is ${c}`)
+
+
+console.log(typeof true)
+console.log(typeof false)*/
