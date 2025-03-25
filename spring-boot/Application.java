b24d87d420cfc327d8907449f18f088fb8019f58
Suhas-MSRUAS <158500939+Suhas-MSRUAS@users.noreply.github.com>
2025-03-26 01:16:57 +0530

    Add files via upload

---
+++ spring-boot/Application.java
@@ -0,0 +1,18 @@
+package com.js.springboot;
+
+import java.util.List;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.js.springboot.dto.Person;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	
+}
